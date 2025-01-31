8618ee73fbdc28bc1b8bd75f0ff84d7060d44821
sanasuno <sanasunozzz@gmail.com>
2025-01-31 18:30:22 +0900

    サイトの初期設定

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   url: 'https://sanasuno.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: isDev ? '/' : '/glore/',  // ローカルでは '/' を使う
+  baseUrl: isDev ? '/' : '/genshinlore/',  // ローカルでは '/' を使う
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
