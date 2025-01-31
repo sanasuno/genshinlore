e3667287a4ddea5639f326b99bd372bf0d2a146e
sanasuno <sanasunozzz@gmail.com>
2025-01-31 16:48:20 +0900

    Initial commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://sanasuno.github.io/',
+  url: 'https://sanasuno.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/genshinlore/',
